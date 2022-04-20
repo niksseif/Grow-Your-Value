6ada88fe9b50460901731f767abc00c0c7397177
Nikss <nik.safe@gmail.com>
2022-04-20 10:31:16 -0700

    add seedpacket-form, and updated state and local storage

--- src/SeedPacket/SeedPacket.js
+++ src/SeedPacket/SeedPacket.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SeedPacket = () => {
-  return (
-    <>
-      <h1>SeedPacket</h1>
-    </>
-  )
+class SeedPacket extends Component{ 
+  constructor() {
+    super();
+    this.state ={
+      name: "",
+    }
+  }
+
+  handleChange(e){
+    this.setState({name: e.target.value})
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.plantSeed(this.state.name)
+  }
+  render(){
+    return (
+      <>
+        <h1>SeedPacket</h1>
+        <div>
+          <form onSubmit={(e) => this.handleSubmit(e) }>
+            <label htmlFor='seed-name'>
+              <input id="seed-name" type="text" onChange={(e) =>this.handleChange(e) }></input>
+              </label>
+            <button>Submit</button> 
+          </form>
+        </div>
+      </>
+    )
+}
 }
 
 export default SeedPacket;
